69daa60996230b0212361087ae68e33454e3dcdb
John Amores <kayak.stated_0b@icloud.com>
2023-05-23 20:29:06 -0400

    Updating templates

--- base/views.py
+++ base/views.py
@@ -4,8 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python!'},
+    {'id': 2, 'name': 'Frontend Development!'},
+    {'id': 3, 'name': 'Backend Development!'},
+]
+
+
 def home(request):
-    return render(request, 'main.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 
 def room(request):
