6b40bb6b7934091998f95aa6489de943516189b2
John Amores <kayak.stated_0b@icloud.com>
2023-05-26 21:05:32 -0400

    Search Functionality started

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -15,7 +15,10 @@ from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all
-    context = {'rooms': rooms}
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
