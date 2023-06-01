c58d80c53e1952d939309e566a433009eb1dfbd1
John Amores <kayak.stated_0b@icloud.com>
2023-05-31 20:55:00 -0400

    Finished User Registration template

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    path('register/', views.registerUser, name="register"),
+
     path('login/', views.loginUser, name="login"),
     path('logout/', views.logoutUser, name="logout"),
 
--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 
 from .models import Room, Topic
@@ -19,10 +20,32 @@ from .forms import RoomForm
 ] """
 
 
-def loginUser(request):
+def registerUser(request):
+    form = UserCreationForm()
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during Registration')
+
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
+
+
+def loginUser(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -38,7 +61,7 @@ def loginUser(request):
         else:
             messages.error(request, 'Username or Password does not exist')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
